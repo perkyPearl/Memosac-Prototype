f730403ec400c8d63137e1a19a5aa8e2cef83795
parth <parthbatra1908@gmail.com>
2025-02-08 17:28:42 +0530

    login route added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoute"); 
 
+dotenv.config();
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => {
-    res.send('Jai kara sherawali da bol saache darbar ki Jai!✨');
+
+app.use(express.json()); 
+app.use(cookieParser()); 
+
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Jai kara sherawali da bol saache darbar ki Jai!✨");
 });
 
+// Start server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`🚀 Server is running on http://localhost:${port}`);
 });
+
+
+
